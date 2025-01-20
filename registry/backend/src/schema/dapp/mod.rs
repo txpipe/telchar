f0612bed80466399d6ddbed2f0e0c3ca5c8cba9a
EehMauro <maurojoaquinescudero@gmail.com>
2025-01-20 18:06:10 -0300

    Updated graphql query

--- registry/backend/src/schema/dapp/mod.rs
+++ registry/backend/src/schema/dapp/mod.rs
@@ -1,4 +1,6 @@
-use async_graphql::{ComplexObject, SimpleObject, ID};
+use async_graphql::{ComplexObject, SimpleObject};
+use telchar_codegen::blueprint::Blueprint;
+use telchar_codegen::{get_blueprint_from_path};
 
 mod query;
 mod json;
@@ -8,24 +10,16 @@ pub use query::{DAppQuery};
 #[derive(SimpleObject, Clone)]
 #[graphql(complex)]
 pub struct DApp {
-    id: ID,
     name: String,
     scope: String,
-    repository: String,
+    repository_url: String,
+    blueprint_url: String,
     published_date: i64,
 }
 
-#[ComplexObject]
-impl DApp {}
-
 #[derive(SimpleObject, Clone)]
 #[graphql(complex)]
-pub struct DAppDetail {
-    id: ID,
-    name: String,
-    scope: String,
-    repository: String,
-    published_date: i64,
+pub struct DAppBlueprint {
     description: String,
     version: String,
     license: String,
@@ -35,4 +29,25 @@ pub struct DAppDetail {
 }
 
 #[ComplexObject]
-impl DAppDetail {}
+impl DApp {
+    async fn blueprint(&self) -> DAppBlueprint {
+        let blueprint: Blueprint = get_blueprint_from_path(format!("../data/{}_{}.json", self.scope, self.name));
+        let mut compiler_name = "".to_string();
+        let mut compiler_version = "".to_string();
+        if let Some(compiler) = blueprint.preamble.compiler {
+            compiler_name = compiler.name;
+            compiler_version = compiler.version.unwrap_or("".to_string());
+        }
+        return DAppBlueprint {
+            description: blueprint.preamble.description.unwrap_or("".to_string()),
+            version: blueprint.preamble.version,
+            license: blueprint.preamble.license.unwrap_or("".to_string()),
+            compiler_name: compiler_name,
+            compiler_version: compiler_version,
+            plutus_version: blueprint.preamble.plutus_version.to_string(),
+        };
+    }
+}
+
+#[ComplexObject]
+impl DAppBlueprint {}
