4b8f2f002309beefd2a41c177177114c347cb969
Mauro Joaquin Escudero <maurojoaquinescudero@gmail.com>
2025-01-21 11:33:12 -0300

    GraphQL improvements (#3)
    * Added CLI PoC for blueprint template creation
    
    * Bulk commit
    
    * Updated graphql query
    
    * Added data json
    
    * Merge
    
    * Added more fields

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,15 +1,18 @@
 use std::env;
 use serde_json;
-use telchar_codegen::{get_blueprint_from_path, get_schemas_from_blueprint, get_template_from_blueprint};
+use telchar_codegen::{get_blueprint_from_path, get_schemas_from_blueprint, get_template_from_blueprint, get_validators_from_blueprint};
 use telchar_codegen::blueprint::Blueprint;
 use telchar_codegen::schema::Schema;
+use telchar_codegen::schema::Validator;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path: &String = &args[1];
     let blueprint: Blueprint = get_blueprint_from_path(path.to_string());
-    //let schemas: Vec<Schema> = get_schemas_from_blueprint(blueprint);
-    //println!("{}", serde_json::to_string_pretty(&schemas).unwrap());
-    let template: String = get_template_from_blueprint(blueprint);
-    println!("{}", template);
+    //let validators: Vec<Validator> = get_validators_from_blueprint(blueprint);
+    //println!("{}", serde_json::to_string_pretty(&validators).unwrap());
+    let schemas: Vec<Schema> = get_schemas_from_blueprint(blueprint);
+    println!("{}", serde_json::to_string_pretty(&schemas).unwrap());
+    //let template: String = get_template_from_blueprint(blueprint);
+    //println!("{}", template);
 }
--- registry/backend/src/schema/dapp/mod.rs
+++ registry/backend/src/schema/dapp/mod.rs
@@ -1,4 +1,8 @@
-use async_graphql::{ComplexObject, SimpleObject, ID};
+use async_graphql::{ComplexObject, SimpleObject};
+use telchar_codegen::{get_blueprint_from_path, get_validators_from_blueprint, get_schemas_from_blueprint};
+use telchar_codegen::blueprint::Blueprint;
+use serde::{Deserialize, Serialize};
+use serde_json;
 
 mod query;
 mod json;
@@ -8,31 +12,100 @@ pub use query::{DAppQuery};
 #[derive(SimpleObject, Clone)]
 #[graphql(complex)]
 pub struct DApp {
-    id: ID,
     name: String,
     scope: String,
-    repository: String,
+    repository_url: String,
+    blueprint_url: String,
     published_date: i64,
+    readme: String,
 }
-
-#[ComplexObject]
-impl DApp {}
 
 #[derive(SimpleObject, Clone)]
 #[graphql(complex)]
-pub struct DAppDetail {
-    id: ID,
-    name: String,
-    scope: String,
-    repository: String,
-    published_date: i64,
+pub struct DAppBlueprint {
     description: String,
     version: String,
     license: String,
     compiler_name: String,
     compiler_version: String,
     plutus_version: String,
+
+    #[graphql(skip)]
+    blueprint: Blueprint,
+}
+
+#[derive(SimpleObject, Deserialize, Serialize, Clone)]
+#[graphql(complex)]
+pub struct DAppValidator {
+    name: String,
+    datum: Option<DAppReference>,
+    redeemer: Option<DAppReference>,
+    parameters: Vec<DAppReference>,
+}
+
+#[derive(SimpleObject, Deserialize, Serialize, Clone)]
+#[graphql(complex)]
+pub struct DAppReference {
+    name: Option<String>,
+    schema_name: String,
+}
+
+#[derive(SimpleObject, Deserialize, Serialize, Clone)]
+#[graphql(complex)]
+pub struct DAppSchema {
+    name: String,
+    schema: String,
 }
 
 #[ComplexObject]
-impl DAppDetail {}
+impl DApp {
+    async fn blueprint(&self) -> DAppBlueprint {
+        let blueprint: Blueprint = get_blueprint_from_path(format!("../data/{}_{}.json", self.scope, self.name));
+        let mut compiler_name = "".to_string();
+        let mut compiler_version = "".to_string();
+        if let Some(compiler) = blueprint.preamble.compiler.clone() {
+            compiler_name = compiler.name;
+            compiler_version = compiler.version.unwrap_or("".to_string());
+        }
+        return DAppBlueprint {
+            description: blueprint.preamble.description.clone().unwrap_or("".to_string()),
+            version: blueprint.preamble.version.clone(),
+            license: blueprint.preamble.license.clone().unwrap_or("".to_string()),
+            compiler_name: compiler_name,
+            compiler_version: compiler_version,
+            plutus_version: blueprint.preamble.plutus_version.clone().to_string(),
+            blueprint: blueprint,
+        };
+    }
+}
+
+#[ComplexObject]
+impl DAppBlueprint {
+    // Improve this
+    async fn validators(&self) -> Vec<DAppValidator> {
+        let validators = get_validators_from_blueprint(self.blueprint.clone());
+        serde_json::from_str(&serde_json::to_string_pretty(&validators).unwrap()).expect("Unable to parse")
+    }
+
+    async fn schemas(&self) -> Vec<DAppSchema> {
+        let mut schemas: Vec<DAppSchema> = vec![];
+        for schema in get_schemas_from_blueprint(self.blueprint.clone()) {
+            schemas.push(
+                DAppSchema {
+                    name: schema.name.clone(),
+                    schema: schema.json.clone(),
+                }
+            );
+        }
+        schemas
+    }
+}
+
+#[ComplexObject]
+impl DAppValidator {}
+
+#[ComplexObject]
+impl DAppReference {}
+
+#[ComplexObject]
+impl DAppSchema {}
