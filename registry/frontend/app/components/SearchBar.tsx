790758d429a6a2966b369329ccfa1107af712a50
Maximiliano Duthey <maxi.duthey@xoor.io>
2025-01-23 13:10:56 -0300

    feat(): add search result display in SearchBar component

--- registry/frontend/app/components/SearchBar.tsx
+++ registry/frontend/app/components/SearchBar.tsx
@@ -12,6 +12,21 @@ interface Props {
   showViewAll?: boolean;
 }
 
+function SearchResult({ dapp }: { dapp: Dapp; }) {
+  return (
+    <Link to={`/dapp/${dapp.scope}/${dapp.name}`} className="text-black/90 hover:bg-primary-400/10 rounded-xl py-1.5 px-4 block">
+      <div className="text-lg font-semibold">{dapp.name}</div>
+      <div>
+        <span className="text-primary-400">@{dapp.scope}</span>
+        <span> • </span>
+        <span>v{dapp.blueprint.version}</span>
+      </div>
+    </Link>
+  );
+}
+
+const dapps: Dapp[] = [];
+
 export function SearchBar({ className, showViewAll }: Props) {
   const [search, setSearch] = useState('');
   const searchRef = useRef<HTMLInputElement>(null);
@@ -44,6 +59,18 @@ export function SearchBar({ className, showViewAll }: Props) {
             searchRef.current?.focus();
           }}
         />
+        {dapps.length > 0 && (
+          <div className="absolute left-0 right-0 -bottom-4 bg-white rounded-xl px-8 py-6 translate-y-full z-10">
+            <span className="text-black/50">Top results</span>
+            <ul className="max-h-64 overflow-y-auto mt-4">
+              {dapps.map(dapp => (
+                <li key={dapp.id}>
+                  <SearchResult dapp={dapp} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </search>
       {showViewAll && (
         <Link to="/" className="text-white/95 underline underline-offset-[3px]">
