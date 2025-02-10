96ca30dd6f281a27912a8dcdc1738b8c04b7b148
Maximiliano <maxi.duthey@txpipe.io>
2025-02-10 16:22:34 -0300

    Feat/improvements (#23)
    * ci(): only execute backend, front and docs actions manually
    
    * feat(): change dotenv to dotenv_codegen

--- cli/src/command/codegen.rs
+++ cli/src/command/codegen.rs
@@ -1,13 +1,11 @@
-use std::env;
-
 use crate::run_codegen_query;
 
 pub async fn run(sub_matches: &clap::ArgMatches) {
-    let graphql_url = env::var("GRAPHQL_URL").expect("GRAPHQL_URL must be set in the environment");
+    let graphql_url = dotenv!("GRAPHQL_URL");
 
     let blueprint_reference = sub_matches.get_one::<String>("blueprint").expect("required");
     if let Some((scope, name)) = blueprint_reference.split_once('/') {
-        let codegen = run_codegen_query(graphql_url, scope.to_string(), name.to_string()).await;
+        let codegen = run_codegen_query(graphql_url.to_string(), scope.to_string(), name.to_string()).await;
         match codegen {
             Some(codegen) => println!("{}", codegen),
             None => println!("Blueprint not found")
