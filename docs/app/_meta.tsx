c7cadd03c9b2bbc503a65b9bc204b3f1785c0b69
Maximiliano <maxi.duthey@txpipe.io>
2025-02-11 10:09:24 -0300

    feat(): add search + oci + publish cli command (#25)
    * feat(): enable search module
    
    * feat(): add oci docs
    
    * feat(): some basic information of cli commands

---
+++ docs/app/_meta.tsx
@@ -0,0 +1,6 @@
+export default {
+  index: '',
+  cli: '',
+  oci: '',
+  backend: '',
+};
--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -7,8 +7,8 @@ export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
   title: {
-    absolute: '',
-    template: '%s - Telchar Docs',
+    template: '%s - Telchar',
+    default: 'Telchar',
   },
 };
 
